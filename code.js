c2f61fa167c0a43de3d89af944ab9c7b0094ccea
AaronLee08329 <88349849+AaronLee08329@users.noreply.github.com>
2021-08-05 09:50:58 -0700

    Update code.js

--- code.js
+++ code.js
@@ -11,3 +11,12 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-50px";
   }
 }
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
